d7b5ac115d1f8a87fd581c14e8c323eff1883eae
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 01:41:52 -0400

    feat: add query client provider to entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './config/react-query-config'
 import './index.css'
 import App from './App'
 import './bootstrap.min.css'
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
