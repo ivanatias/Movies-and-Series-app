3ee7977957b39f6611099b6ae0e440402e5189bd
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 01:42:49 -0400

    feat: add router provider to entry point

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './config/react-query-config'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './config/rrd-router'
 import './index.css'
-import App from './App'
 import './bootstrap.min.css'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 )
