85db497e69ad6d20eda8999da3941ef03fe7767a
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 01:37:32 -0400

    feat: add initial browser router config

---
+++ src/config/rrd-router.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../components/Layout/Layout'
+import LandingPage from '../Pages/LandingPage'
+import MoviesPage from '../Pages/MoviesPage'
+import SeriesPage from '../Pages/SeriesPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />
+      },
+      {
+        path: 'movies',
+        element: <MoviesPage />
+      },
+      {
+        path: 'tvseries',
+        element: <SeriesPage />
+      }
+    ]
+  }
+])
+
+export { router }
