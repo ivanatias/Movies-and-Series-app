26e596702fbcd37f5a98eebaddba57773bf00890
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 04:07:14 -0400

    refactor: remove error catching from services to let react query catch them

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,15 +3,11 @@ import axios from 'axios'
 const API = 'https://api.themoviedb.org/3'
 
 export const getData = async endpoint => {
-  try {
-    const response = await axios.get(API + endpoint, {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-        'Content-Type': 'application/json;charset=utf-8'
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
+  const { data } = await axios.get(API + endpoint, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  })
+  return data
 }
--- src/utils/getInfiniteData.js
+++ src/utils/getInfiniteData.js
@@ -3,15 +3,11 @@ import axios from 'axios'
 const API = 'https://api.themoviedb.org/3'
 
 export const getInfiniteData = async (endpoint, page) => {
-  try {
-    const response = await axios.get(API + endpoint + '&page=' + page, {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-        'Content-Type': 'application/json;charset=utf-8'
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
+  const { data } = await axios.get(API + endpoint + '&page=' + page, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  })
+  return data
 }
