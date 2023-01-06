439325622524180b0f12df52cb97176ef1b1aea4
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 02:33:07 -0400

    render tv series page

--- src/Pages/SeriesPage.jsx
+++ src/Pages/SeriesPage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import TVGrid from '../components/TV/TVGrid'
-import { useRouteQuery } from '../hooks/useRouteQuery'
-import { useDebounce } from '../hooks/useDebounce'
+/* import { useRouteQuery } from '../hooks/useRouteQuery'
+import { useDebounce } from '../hooks/useDebounce' */
 
 const SeriesPage = () => {
-  const query = useRouteQuery()
+  /*   const query = useRouteQuery()
   const search = query.get('search')
 
-  const debouncedSearch = useDebounce(search, 500)
+  const debouncedSearch = useDebounce(search, 500) */
 
-  return <TVGrid key={debouncedSearch} search={debouncedSearch} />
+  return <TVGrid />
 }
 
 export default SeriesPage
