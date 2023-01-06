2b1c084d2234549d590d591af1c0f8f48878522f
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 01:32:01 -0400

    refactor: modify entry point for react v18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import './bootstrap.min.css'
 
-ReactDOM.render(
-  <App />,
+const root = createRoot(document.getElementById('root'))
 
-  document.getElementById('root')
-)
+root.render(<App />)
