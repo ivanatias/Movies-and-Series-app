f122a3b98495ac29cece08a0e22ecaaaa5dd28e7
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 02:56:29 -0400

    refactor: remove unneeded function from DetailsModal component

--- src/components/Details/DetailsModal.jsx
+++ src/components/Details/DetailsModal.jsx
@@ -3,20 +3,16 @@ import { Modal, Container } from 'react-bootstrap'
 import Info from './Info'
 
 const DetailsModal = ({ item, showModal, handleClose }) => {
-  //Checks if it's a movie or a tv serie, if the item has "release date" then it's a movie, if not, it's a tv serie
-  const checkMovieOrTv = () => {
-    let movieOrTv = item.release_date ? 'movie' : 'tv'
-    return movieOrTv
-  }
+  const itemType = item.release_date !== undefined ? 'movie' : 'tv'
 
   return (
     <Modal fullscreen show={showModal} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>{item.title || item.name}</Modal.Title>
+        <Modal.Title>{item.title ?? item.name}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Container>
-          <Info item={item} movieOrTv={checkMovieOrTv()} />
+          <Info item={item} movieOrTv={itemType} />
         </Container>
       </Modal.Body>
     </Modal>
