59d2b7c7fb01c109403b89585ab310373046c740
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-06 02:51:40 -0400

    refactor: useFetchDetails hook now uses react-query new version

--- src/hooks/useFetchDetails.js
+++ src/hooks/useFetchDetails.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useQuery } from '@tanstack/react-query'
 import { getData } from '../utils/getData'
 
 export const useFetchDetails = (endpoint, queryKey, id) => {
