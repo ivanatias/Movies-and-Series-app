acd8fc1a313fc211d8069643cfe36f29cf77b813
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-09 20:45:56 -0400

    refactor: add react-query dev tools to entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { queryClient } from './config/react-query-config'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './config/rrd-router'
@@ -12,5 +14,6 @@ const root = createRoot(document.getElementById('root'))
 root.render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 )
