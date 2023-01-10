88eb49d1f3a2f8ce619374f3491c612c60502985
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-10 01:40:10 -0400

    refactor(DailyTrendings): implement useData hook

--- src/components/Trendings/DailyTrendings.jsx
+++ src/components/Trendings/DailyTrendings.jsx
@@ -1,30 +1,19 @@
 import React from 'react'
 import Grid from '../Grid/Grid'
 import { Container } from 'react-bootstrap'
-import Loading from '../Loading/Loading'
-import Error from '../Error/Error'
-import { getData } from '../../utils/getData'
-import { useQuery } from '@tanstack/react-query'
+import { useData } from '../../hooks/useData'
 
 const DailyTrendings = ({ initialData }) => {
-  const { data, status } = useQuery(
-    ['trendingsdaily'],
-    () => getData('/trending/all/day'),
-    {
-      initialData
-    }
-  )
+  const { data } = useData({
+    endpoint: '/trending/all/day',
+    queryKey: ['trendingsdaily'],
+    initialData
+  })
 
   return (
     <Container className='pb-5'>
       <h2 className='text-center text-white mb-5'>Daily Trendings</h2>
-      {status === 'loading' ? (
-        <Loading />
-      ) : status === 'error' ? (
-        <Error />
-      ) : (
-        <Grid gridItems={data.results} />
-      )}
+      <Grid gridItems={data.results} />
     </Container>
   )
 }
