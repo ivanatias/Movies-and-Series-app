726a5995364b408565ba097d82f00425bbca7aee
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-10 02:08:20 -0400

    refactor(useData): modify passing args to getData according to named params

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,9 +2,13 @@ import { useQuery } from '@tanstack/react-query'
 import { getData } from '../utils/getData'
 
 export const useData = ({ endpoint, queryKey, initialData = undefined }) => {
-  const { data, status } = useQuery([...queryKey], () => getData(endpoint), {
-    initialData
-  })
+  const { data, status } = useQuery(
+    [...queryKey],
+    ({ signal }) => getData({ endpoint, signal }),
+    {
+      initialData
+    }
+  )
 
   return { data, status }
 }
