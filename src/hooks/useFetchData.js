53181d297e86a3a39fabbd265cbeed645f51d651
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-09 20:37:19 -0400

    refactor(useFetchData): provide initial data option

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -2,7 +2,13 @@ import { useEffect } from 'react'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { getInfiniteData } from '../utils/getInfiniteData'
 
-export const useFetchData = (endpoint, queryKey, query1, query2) => {
+export const useFetchData = (
+  endpoint,
+  queryKey,
+  query1,
+  query2,
+  initialData
+) => {
   const { data, status, isFetchingNextPage, fetchNextPage } = useInfiniteQuery(
     [`${queryKey}`, query1, query2],
     ({ pageParam: page = 1, signal }) => {
@@ -12,13 +18,15 @@ export const useFetchData = (endpoint, queryKey, query1, query2) => {
       getNextPageParam: lastPage => {
         const { page, total_pages: totalPages } = lastPage
         return page < totalPages ? page + 1 : undefined
-      }
+      },
+      initialData
     }
   )
 
   useEffect(() => {
     const onScroll = async event => {
-      const { scrollHeight, scrollTop, clientHeight } = event.target.scrollingElement
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement
       if (scrollHeight - scrollTop <= clientHeight * 1.2) {
         await fetchNextPage()
       }
