737b863d4b9291dd41dc1a201cf54f794f2401e1
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-10 21:36:07 -0400

    feat(Search): add loading indicator based on search strings

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect, useRef, memo } from 'react'
 import { Row, Col, Form as BsForm } from 'react-bootstrap'
 import { FaSearch } from 'react-icons/fa'
-import { Form } from 'react-router-dom'
+import { Form, useNavigation } from 'react-router-dom'
 
 const Search = ({ type, initialSearchValue }) => {
+  const navigation = useNavigation()
   const searchInputRef = useRef(null)
 
   useEffect(() => {
     searchInputRef.current.value = initialSearchValue
   }, [initialSearchValue])
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('search')
+
   return (
     <Row>
       <Col md={6} className='mx-auto mb-1'>
         <Form id='search-form' role='search' className='position-relative'>
           <FaSearch
-            className='position-absolute top-50 end-0 translate-middle text-dark'
+            className={`position-absolute top-50 end-0 translate-middle ${
+              searching ? 'text-danger' : 'text-dark'
+            }`}
             size='20'
           />
           <BsForm.Group
             className='mb-3'
-            controlId={type === 'movie' ? 'Search Movie' : 'Search TV Serie'}
+            controlId={type === 'movie' ? 'searchmovie' : 'searchtv'}
           >
             <BsForm.Control
               ref={searchInputRef}
