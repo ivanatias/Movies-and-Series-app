399d5c21fcc2801db84ad061917f56d631a6f937
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-10 23:29:18 -0400

    refactor(useInfiniteScroll): use new syntax on useInfiniteQuery hook

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -3,19 +3,17 @@ import { useInfiniteQuery } from '@tanstack/react-query'
 import { getInfiniteData } from '../utils/getInfiniteData'
 
 export const useInfiniteScroll = ({ endpoint, queryKey, initialData }) => {
-  const { data, status, isFetchingNextPage, fetchNextPage } = useInfiniteQuery(
-    [...queryKey],
-    ({ pageParam: page = 1, signal }) => {
+  const { data, status, isFetchingNextPage, fetchNextPage } = useInfiniteQuery({
+    queryKey,
+    queryFn: ({ pageParam: page = 1, signal }) => {
       return getInfiniteData({ endpoint, page, signal })
     },
-    {
-      getNextPageParam: lastPage => {
-        const { page, total_pages: totalPages } = lastPage
-        return page < totalPages ? page + 1 : undefined
-      },
-      initialData
-    }
-  )
+    getNextPageParam: lastPage => {
+      const { page, total_pages: totalPages } = lastPage
+      return page < totalPages ? page + 1 : undefined
+    },
+    initialData
+  })
 
   useEffect(() => {
     const onScroll = async event => {
